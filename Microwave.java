c23502c99af6e5acb1fe1f11d7f81c6b58b865f3
Ioana Marinescu <ioana.marinescu@stu.ocsb.ca>
2024-03-01 00:26:11 +0000

    program can get user input

--- Microwave.java
+++ Microwave.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
-
 /**
  * This program calculates how long 
  * something should be in the microwave for.
  *
  * @author Ioana Marinescu
  * @version 1.0
- * @since 2024-02-25
+ * @since 2024-02-29
  */
-public final class Einstein {
+public final class Microwave {
   /** Private constructor to prevent instantiation of this utility class. */
-  private Einstein() { }
+  private Microwave() { }
 
   /**
    * The main method prints a stylish greeting message.
@@ -18,43 +17,24 @@ public final class Einstein {
    * @param args Command-line arguments (not used in this program)
    */
   public static void main(final String[] args) {
-    // gets user input (pt.1)
+    // scanner creation
     final Scanner scanner = new Scanner(System.in);
-    System.out.println("What is the mass of your object (kg):");
 
-    // variable declaration
-    final double mass;
+    // user input for food
+    System.out.println("What would you like to heat up? (sub, pizza, soup)");
+    final String userFood = scanner.nextLine();
+    // user input for amount of food
+    System.out.println("How many are you heating up? (1-3)");
+    final String userAmountStr = scanner.nextLine();
 
     try {
-      // gets user input (pt.2)
-      final String massStr = scanner.nextLine();
-
-      // converts string to double
-      mass = Double.parseDouble(massStr);
-
-      // checks if user input is positive
-      if (mass > 0) {
-        // initialization of speed of light
-        final int speedOfLight = 299792458;
-
-        // calculation of energy
-        final double energy = mass * Math.pow(speedOfLight, 2);
-
-        // output result to console
-        System.out.format("The energy released is %.3f", energy);
-        System.out.println();
-
-        // user imputed a negative or null mass
-      } else {
-        System.out.println("Invalid input, please enter"
-        + " a positive integer or decimal input.");
-      }
+      // converts string to int and store in variable
+      int userAmount = Integer.parseInt(userAmountStr);
 
       // user did not enter in a double input
     } catch (NumberFormatException error) {
       // displays error message
-      System.out.println("Invalid input, please enter"
-      + " a positive integer or decimal input.");
+      System.out.println("That is not a valid amount of food to put in the microwave!");
     }
 
     // closes scanner
